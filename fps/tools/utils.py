6f8b4f58a5c30ff952effbd0859ebb86f89095be
haolamnm <haolamnm@gmail.com>
2025-08-12 00:18:09 +0700

    refactor function

--- fps/tools/utils.py
+++ fps/tools/utils.py
@@ -1,5 +1,4 @@
 import subprocess
-from typing import Any
 
 from ..services.common.utils import get_logger
 from .constants import FAILURE_EXIT_CODE, SUCCESS_EXIT_CODE
@@ -7,17 +6,9 @@ from .constants import FAILURE_EXIT_CODE, SUCCESS_EXIT_CODE
 logger = get_logger(__name__)
 
 
-def run_command(
-    command: list[str], video_id: str, description: str = "running command", env: dict[str, Any] | None = None
-) -> int:
+def run_command(command: list[str], video_id: str | None, description: str = "running command") -> int:
     try:
-        subprocess.run(
-            command,
-            env=env,
-            text=True,
-            check=True,
-            capture_output=True,
-        )
+        subprocess.run(command, text=True, check=True, capture_output=True)
         return SUCCESS_EXIT_CODE
     except subprocess.CalledProcessError as e:
         logger.error(f"Error {description} for video {video_id}: {e.stderr.strip()}")
